56be3257c8cda62417b6badaf4d838844904958c
Nicolas Luis Moura de Almeida <nicolasluis215@gmail.com>
2026-04-22 09:06:04 -0300

    NPSV-319 feat: adiciona uma quebra de linha ao final do arquivo .firebaserc para manter a consistência

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ProductsPage from "./pages/products/ProductsPage";
 import FormulasPage from "./pages/formulas/FormulasPage";
 import ProductionHistoryPage from "./pages/productions/ProductionsPage";
 import SalesHistoryPage from "./pages/sales/SalesPage";
-import ClientsPage from "./pages/sales/ClientsPage";
+import ClientsPage from "./pages/clients/ClientsPage";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
